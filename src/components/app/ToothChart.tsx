a2d295c89f98902d3412ae9fd934bd096addf211
Jawad Jasmine <jawadjasmind@gmail.com>
2025-07-19 17:22:33 +0000

    perfect now align the charts properly in the tooth chart and notes box

--- src/components/app/ToothChart.tsx
+++ src/components/app/ToothChart.tsx
@@ -138,16 +138,20 @@ export const ToothChart: React.FC<ToothChartProps> = ({ value, onChange }) => {
                 {/* Primary Teeth */}
                 <h3 className="text-sm font-medium text-center">Primary Dentition</h3>
                 <div className="flex flex-col items-center gap-2">
-                    <div className="flex justify-center gap-1 md:gap-2">
-                        <Quadrant teeth={primaryUpperRight} notes={value} onNoteChange={handleNoteChange} isPrimary reverse/>
-                        <div className="border-l-2 border-gray-400 mx-1"></div>
-                        <Quadrant teeth={primaryUpperLeft} notes={value} onNoteChange={handleNoteChange} isPrimary reverse />
+                    <div className="flex justify-center w-full">
+                        <div className="flex justify-center gap-1 md:gap-2">
+                           <Quadrant teeth={primaryUpperRight} notes={value} onNoteChange={handleNoteChange} isPrimary reverse/>
+                           <div className="border-l-2 border-gray-400 mx-1"></div>
+                           <Quadrant teeth={primaryUpperLeft} notes={value} onNoteChange={handleNoteChange} isPrimary reverse />
+                        </div>
                     </div>
                      <div className="border-b-2 border-gray-400 my-2 w-full max-w-sm mx-auto"></div>
-                    <div className="flex justify-center gap-1 md:gap-2">
-                        <Quadrant teeth={primaryLowerRight} notes={value} onNoteChange={handleNoteChange} isPrimary reverse/>
-                        <div className="border-l-2 border-gray-400 mx-1"></div>
-                        <Quadrant teeth={primaryLowerLeft} notes={value} onNoteChange={handleNoteChange} isPrimary />
+                     <div className="flex justify-center w-full">
+                        <div className="flex justify-center gap-1 md:gap-2">
+                           <Quadrant teeth={primaryLowerRight} notes={value} onNoteChange={handleNoteChange} isPrimary reverse/>
+                           <div className="border-l-2 border-gray-400 mx-1"></div>
+                           <Quadrant teeth={primaryLowerLeft} notes={value} onNoteChange={handleNoteChange} isPrimary />
+                        </div>
                     </div>
                 </div>
             </div>
