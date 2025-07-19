6d4076e17b54a9ce1f4237069f90d5005b0da87d
Jawad Jasmine <jawadjasmind@gmail.com>
2025-07-19 20:52:24 +0000

    this is actually what I want

--- src/components/icons/ToothIcon.tsx
+++ src/components/icons/ToothIcon.tsx
@@ -6,23 +6,16 @@ export function ToothIcon(props: SVGProps<SVGSVGElement>) {
       {...props}
       width="24"
       height="24"
-      viewBox="0 0 64 64"
+      viewBox="0 0 50 50"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path
-        d="M32 4 C24 4, 16 12, 16 26 C16 40, 22 60, 28 60 C30 60, 30 48, 32 48 C34 48, 34 60, 36 60 C42 60, 48 40, 48 26 C48 12, 40 4, 32 4 Z"
-        fill="none"
+      <path 
+        d="M42.1,19.2c0-7.9-3.7-11-9.4-11c-4.7,0-7.8,2.7-9.7,5.4c-1.9-2.7-5-5.4-9.7-5.4c-5.7,0-9.4,3.1-9.4,11c0,7.7,6.3,12.5,10.3,17.6c3.1,4,5.4,6.7,5.4,6.7c0.8,0.9,2.4,0.9,3.2,0c0,0,2.3-2.7,5.4-6.7C35.8,31.7,42.1,26.9,42.1,19.2z"
         stroke="currentColor"
-        strokeWidth="3"
+        strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
-      />
-      <path
-        d="M24 16C30 12 36 12 40 16"
-        stroke="currentColor"
-        strokeWidth="2"
-        fill="none"
-        strokeLinecap="round"
       />
     </svg>
   );
