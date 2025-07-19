3b4dc97132e327781aae868d35eaca3285fd7b11
Jawad Jasmine <jawadjasmind@gmail.com>
2025-07-19 20:53:05 +0000

    nop not accurate

--- src/components/icons/ToothIcon.tsx
+++ src/components/icons/ToothIcon.tsx
@@ -4,19 +4,18 @@ export function ToothIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
-      width="24"
-      height="24"
-      viewBox="0 0 50 50"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
-      <path 
-        d="M42.1,19.2c0-7.9-3.7-11-9.4-11c-4.7,0-7.8,2.7-9.7,5.4c-1.9-2.7-5-5.4-9.7-5.4c-5.7,0-9.4,3.1-9.4,11c0,7.7,6.3,12.5,10.3,17.6c3.1,4,5.4,6.7,5.4,6.7c0.8,0.9,2.4,0.9,3.2,0c0,0,2.3-2.7,5.4-6.7C35.8,31.7,42.1,26.9,42.1,19.2z"
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <path d="M9.34 2.45 11 6.3l.05.15a1.86 1.86 0 0 0 1.9 1.5h.02a1.86 1.86 0 0 0 1.9-1.5l.05-.15L14.66 2.45" />
+      <path d="M20 8.79a5.12 5.12 0 0 0-5.12-5.12h-5.76A5.12 5.12 0 0 0 4 8.79v5.42A5.12 5.12 0 0 0 9.12 19.3h.01a5.12 5.12 0 0 0 5.11-4.89 1.16 1.16 0 0 1 2.22-.44h.01a5.12 5.12 0 0 0 3.53-4.18V8.79Z" />
+      <path d="M11 12.5a3.5 3.5 0 0 0 0 7" />
+      <path d="M13 12.5a3.5 3.5 0 0 1 0 7" />
     </svg>
   );
 }
